4281add034046be596a31ea9bcafba02d67d89c9
oblute <olivia.blute@gmail.com>
2020-12-04 13:19:47 -0500

    remove patch, add export variable to build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
+  export MAKEFLAGS="CPPFLAGS=-I$PROJ_INC CFLAGS=-I$PROJ_INC"
   mv DESCRIPTION DESCRIPTION.old
   grep -v '^Priority: ' DESCRIPTION.old > DESCRIPTION
   $R CMD INSTALL --build .
