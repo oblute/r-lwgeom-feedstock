983f782ca20eff513c32cf0e1cb5307867a44414
oblute <olivia.blute@gmail.com>
2020-12-10 12:43:10 -0500

    export a path

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export MAKEFLAGS="CPPFLAGS=-I$PROJ_INC CFLAGS=-I$PROJ_INC"
+export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
 
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]] || [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
